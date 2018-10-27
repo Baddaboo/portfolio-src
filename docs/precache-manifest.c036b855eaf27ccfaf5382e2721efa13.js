398002deb0a05c84f03fe1c50508713d36655aed
Blake Tsuzaki <blake589t@gmail.com>
2018-10-27 04:27:07 -0700

    Update

---
+++ docs/precache-manifest.c036b855eaf27ccfaf5382e2721efa13.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b798d79afbd01224adbba1ee26be091",
+    "url": "/img/bsod.5b798d79.png"
+  },
+  {
+    "revision": "1abb4438fd58b0183e4223e70bf0c03f",
+    "url": "/style.css"
+  },
+  {
+    "revision": "cd4c308e3c574dd673c84672084234fc",
+    "url": "/img/prahvi-hand.cd4c308e.png"
+  },
+  {
+    "revision": "e73a8d7bf44d58ee8694",
+    "url": "/css/chunk-vendors.22a847f7.css"
+  },
+  {
+    "revision": "bf40030bddf109c0358d5fd7592771bd",
+    "url": "/img/susan-fg.bf40030b.png"
+  },
+  {
+    "revision": "5c5cf63897ae27bc09a4a3ebf91917f3",
+    "url": "/img/ck-logo.5c5cf638.png"
+  },
+  {
+    "revision": "ad53e688add6d8bd3fef309e9bc04817",
+    "url": "/img/prahvi-scrub.ad53e688.gif"
+  },
+  {
+    "revision": "874600c8779c82a70f150402a97f26da",
+    "url": "/img/prahvi-proto-rough.874600c8.png"
+  },
+  {
+    "revision": "297d31ab40bc3c8fd51f998ed1645803",
+    "url": "/img/prahvi-cover.297d31ab.png"
+  },
+  {
+    "revision": "01eb5d58b7f3b2c144dd1c75f63c6f20",
+    "url": "/img/prahvi-headset.01eb5d58.png"
+  },
+  {
+    "revision": "6ebbf75018edea95b676ba815efcfea6",
+    "url": "/img/prahvi-logo.6ebbf750.png"
+  },
+  {
+    "revision": "26fc5d8d40d6368f6c56dc9b5c0ac283",
+    "url": "/img/prahvi-findings.26fc5d8d.png"
+  },
+  {
+    "revision": "0a5bd47b4eddae64d7e5a73e5f093b61",
+    "url": "/img/wsos-logo.0a5bd47b.png"
+  },
+  {
+    "revision": "c2dd9e2398473da666c4e3d8e3cd20d9",
+    "url": "/img/spotify-files.c2dd9e23.png"
+  },
+  {
+    "revision": "898453fb7adc4f9bd3d846bd5b2afc49",
+    "url": "/img/profile.898453fb.jpg"
+  },
+  {
+    "revision": "58fd67c377930773594ad9ee55d07810",
+    "url": "/img/easylayout-fg.58fd67c3.png"
+  },
+  {
+    "revision": "c4a6e912ed66e635f1f72936f13fd199",
+    "url": "/img/workday-bg.c4a6e912.png"
+  },
+  {
+    "revision": "61839b9cd17c734a26f47f7a22f68e87",
+    "url": "/img/yelp-logo.61839b9c.png"
+  },
+  {
+    "revision": "504a9d85ae161f866f21ddd56cbbe9d6",
+    "url": "/img/prahvi-diagram.504a9d85.png"
+  },
+  {
+    "revision": "268ae229ce97f6b6fadec7c6cc3f88d0",
+    "url": "/img/prahvi-playback.268ae229.gif"
+  },
+  {
+    "revision": "e14e68d4457c100be2b39c150ab43460",
+    "url": "/img/tiger-logo.e14e68d4.png"
+  },
+  {
+    "revision": "17bfec77814921159c1b7746edb8f361",
+    "url": "/img/prahvi-bg.17bfec77.png"
+  },
+  {
+    "revision": "a274e21bd2bbf55580ae7d1ac402cb05",
+    "url": "/img/prahvi-rpi-screen.a274e21b.png"
+  },
+  {
+    "revision": "e9fffe39433e72806716942bd803e1ae",
+    "url": "/img/hermes-fg.e9fffe39.png"
+  },
+  {
+    "revision": "43f0c548d5f69505ee83",
+    "url": "/js/app.a3d0a8db.js"
+  },
+  {
+    "revision": "e73a8d7bf44d58ee8694",
+    "url": "/js/chunk-vendors.079ade78.js"
+  },
+  {
+    "revision": "846c7df459b2f945d09a729c7d3df4b1",
+    "url": "/img/wsos-bg.846c7df4.png"
+  },
+  {
+    "revision": "0f153383808bf1c8cf9dd2b6d778626b",
+    "url": "/img/prahvi-screenshot.0f153383.png"
+  },
+  {
+    "revision": "96bf7a11bea40d809f786ee030de1d46",
+    "url": "/img/facebook-fg.96bf7a11.png"
+  },
+  {
+    "revision": "c3a178c44f9f75b673a089559e06b497",
+    "url": "/img/google-bg.c3a178c4.png"
+  },
+  {
+    "revision": "a760194ced72846764c86c778eaf91c1",
+    "url": "/img/amazon-fg.a760194c.png"
+  },
+  {
+    "revision": "9bba669808e731c9ec275d9064107cbb",
+    "url": "/img/easylayout-bg.9bba6698.png"
+  },
+  {
+    "revision": "830afc088c555b79f4282637b9c64ddd",
+    "url": "/img/prahvi-process.830afc08.png"
+  },
+  {
+    "revision": "00084bfcd0f5b0cec4f6ebc6cdd2c41e",
+    "url": "/img/spotify-setup.00084bfc.png"
+  },
+  {
+    "revision": "e77294cb88de52e9b6fe3ce700cd17bd",
+    "url": "/img/hermes-bg.e77294cb.png"
+  },
+  {
+    "revision": "d4335d1185641301525b6c0722746249",
+    "url": "/img/prahvi-vui.d4335d11.png"
+  },
+  {
+    "revision": "0c7df0da68e6a57efeaec16852c2f465",
+    "url": "/img/yelp-bg.0c7df0da.jpg"
+  },
+  {
+    "revision": "c69865318225815d876beb873e298e0e",
+    "url": "/img/workday-logo.c6986531.png"
+  },
+  {
+    "revision": "ebe0ee9914aecde94b9bfacf7011d082",
+    "url": "/img/scroll-example.ebe0ee99.png"
+  },
+  {
+    "revision": "c26aef779cf2bddd36124723ed5ee491",
+    "url": "/img/prahvi-ocr.c26aef77.png"
+  },
+  {
+    "revision": "fbd52be277fece7699738d726ff5b247",
+    "url": "/img/google-fg.fbd52be2.png"
+  },
+  {
+    "revision": "ff15cbe6cabfa2233f7411c12c53f3df",
+    "url": "/img/classickit-cover.ff15cbe6.png"
+  },
+  {
+    "revision": "74d1746df700f22331fdd9a150b59ac3",
+    "url": "/img/prahvi-alicia.74d1746d.png"
+  },
+  {
+    "revision": "dc1d63c69d5f5f140ff9473e874d9cdd",
+    "url": "/img/ck-bg.dc1d63c6.png"
+  },
+  {
+    "revision": "8d73244e1d0ceaa949a09a364316be79",
+    "url": "/img/tiger-bg.8d73244e.png"
+  },
+  {
+    "revision": "4ee7458f45524028717d7ba029adaa2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "43f0c548d5f69505ee83",
+    "url": "/css/app.21e3b625.css"
+  }
+];
